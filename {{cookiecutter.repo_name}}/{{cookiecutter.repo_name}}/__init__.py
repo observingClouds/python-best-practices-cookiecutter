4eecb5e98b9fce5f681c6dcff6aa1d1fe406dd17
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2020-12-25 17:41:38 +0100

    \ Black

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -1,7 +1,4 @@
 # Create __version__ attribute from setup.py information
-import os.path
-
-from pkg_resources import DistributionNotFound, get_distribution
 
 from ._version import get_versions
 
@@ -9,5 +6,5 @@ __import__("pkg_resources").declare_namespace(__name__)
 
 try:
     __version__ = get_versions()["version"]
-except:
+except (ValueError, ImportError):
     __version__ = "--"
